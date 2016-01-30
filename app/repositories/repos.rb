8b0024694f4b9c10cdfedee47cb6b64dbf93d3f7
JT Archie <jtarchie@gmail.com>
2016-01-30 16:50:18 -0500

    remote repo and update pipeline

--- app/repositories/repos.rb
+++ app/repositories/repos.rb
@@ -6,7 +6,6 @@ module Repositories
       [
         model.new(uri: 'https://github.com/LinuxBozo/brij-spec'),
         model.new(uri: 'https://github.com/flavorjones/mini_portile'),
-        model.new(uri: 'https://github.com/jtarchie/concourse-saas'),
         model.new(uri: 'https://github.com/jtarchie/pullrequest-resource'),
       ]
     end
